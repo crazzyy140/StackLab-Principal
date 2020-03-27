0aaf5ea1123b3915c2edf8ec0eb14e3ff30d7fe6
young-js <62726410+young-js@users.noreply.github.com>
2020-03-26 23:04:54 -0300

    Brinque de dado

---
+++ comandos/dado.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js'); // puxando a livraria 'discord.js'
+
+exports.run = (client, message, args) => { // setando a base
+
+    // puxando um chao, com sistema randomico (de 6 a 1)
+    var numero = Math.floor(Math.random() * 6) + 1;
+
+    message.channel.send(`Você jogou o dado e... Ele caiu em: ${numero}`)
+}
+
+exports.help = { // setando o nome do arquivo, seguido do prefix
+    name: 'dado'
+}
