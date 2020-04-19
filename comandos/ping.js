be4fcb5b1e6c32f2546c0a87e965d042160a135e
young-js <62726410+young-js@users.noreply.github.com>
2020-04-19 14:47:29 -0300

    Delete ping.js

--- comandos/ping.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js'); // puxando a livraria 'discord.js'
-
-exports.run = (client, message, args) => { // setando as bases
-                                        
-    message.channel.send(`Minha latência está em: \`${parseInt(client.ping)}\` ms`)
-}
-exports.help = { // setando o nome do arquivo, seguido do prefix
-    name: 'ping'
-}
