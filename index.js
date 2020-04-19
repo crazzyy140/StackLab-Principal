9db0a57c37256b22b5766ce96af6d39cd5895526
young-js <62726410+young-js@users.noreply.github.com>
2020-04-19 16:06:25 -0300

    Arquivo main do bot

--- index.js
+++ index.js
@@ -3,16 +3,20 @@ const config = require("./config.json");
 const fs = require("fs");
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
 
 fs.readdir("./comandos/", (err, files) => {
     if (err) console.error(err);
 
-    let arquivojs = files.filter(f => f.split(".").pop() === "js");
-    arquivojs.forEach((f, i) => {
-        let props = require(`./comandos/${f}`);
-        console.log(`Comando ${f} inicou com sucesso`)
-        client.commands.set(props.help.name, props);
+  let arquivojs = files.filter(f => f.split(".").pop() == "js");
+  arquivojs.forEach((f, i) => {
+    let props = require(`./comandos/${f}`);
+    console.log(`${f} - Comando iniciado`);
+    client.commands.set(props.help.name, props);
+    props.help.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
     });
+  });
 });
 
 // um evento ready, que traduzindo, fica 'pronto'. Como ele diz, iremos criar o evento para verificar se o bot está pronto para ligar.
@@ -26,7 +30,7 @@ client.on('ready', () => { // setando o evento com nosso Discord.Client
         {name: 'Minecraft', type: 'PLAYING'}, 
         {name: 'Spotify', type: 'LISTENING'},
         {name: 'YouTube', type: 'WATCHING'},
-        {name: 'amor para todos!', type: 'STREAMING', url: 'https://twitch.tv/younguei'}
+        {name: 'amor para todos!', type: 'STREAMING', url: 'https://twitch.tv/olimpiioo'}
     ];
 // criando uma function...
     function setStatus() { // nomeamos ela de: setStatus
@@ -37,26 +41,25 @@ client.on('ready', () => { // setando o evento com nosso Discord.Client
     setStatus(); // para finalizar, puxamos a function que criamos no inicio
     setInterval(() => setStatus(), 15000) // e adicionamos um intervalo entre as presences
 });  
-
 client.on('guildMemberAdd', membro => { // definimos o nome desse evento, como: membro
-    var canal = client.channels.get("id do canal"); // puxamos o ID do canal, onde enviaremos a embed, para o canal de boas-vindas
-    var cargo = membro.guild.roles.get("id do cargo"); // puxamos o ID de um cargo, no qual, iremos adicionar para o membro
+    var canal1 = client.channels.get("692196839482458232"); // puxamos o ID do canal, onde enviaremos a embed, para o canal de boas-vindas
+    var cargo = membro.guild.roles.get("691904146357616660"); // puxamos o ID de um cargo, no qual, iremos adicionar para o membro
+    var canal = client.channels.get("692056639729041459"); // puxando mais um canal, aonde iremos enviar um sistema de CAPTCHA!
 
 // criando a embed de boas-vindas!
     let embedi = new Discord.RichEmbed()
     
-    .setTitle(`WELCOME`) 
+    .setTitle(`:wave: BEM-VINDO`) 
     .setDescription(`Olá **${membro.user.username}**, seja muito bem vindo (ou vinda) ao \`Discord Lab 🧪\`! Somos uma comunidade focada no aprendizado e auxílio aos programadores, os quais estão sempre ativos para ajudar você também. Esperamos que goste, estamos à disposição! ✨`) 
     .setThumbnail(membro.displayAvatarURL)
     .setColor('BLUE')
-    .addField(`__**SOBRE NÓS**__`, `:busts_in_silhouette: Usuários: \`${membro.guild.memberCount}\`\n<:DL_bot:693959313198153820> Meu prefixo é: \`${config.prefix}\`\n<:DL_github:693136690801410178> Repositório: [Discord-Lab](https://github.com/young-js) \n<:DL_twitter:693132106255040671> Twitter: [@Discord_Lab](https://twitter.com/Discord_Lab)`)
-    .setFooter(`ID do Usuário: ${membro.id}`)
+    .addField(`**SOBRE**`, `:busts_in_silhouette: Usuários: \`${membro.guild.memberCount}\`\n<:DL_github:693136690801410178> Repositório: [Discord-Lab](https://github.com/young-js) \n<:DL_twitter:693132106255040671> Twitter: [@Discord_Lab](https://twitter.com/Discord_Lab)`)
     
     canal.send(embedi)
 });
 
 client.on('guildMemberRemove', membro => { // setamos o nome de membro
-    var canal = client.channels.get("id do canal"); // puxando o ID de um canal para enviar a mensagem
+    var canal = client.channels.get("692056639729041459"); // puxando o ID de um canal para enviar a mensagem
     var server = client.guilds.get
     // criando uma embed
     let embed = new Discord.RichEmbed()
@@ -69,21 +72,41 @@ client.on('guildMemberRemove', membro => { // setamos o nome de membro
     canal.send(`**${membro.user.tag}**`, embed)
 
 });
+
+client.on('guildMemberRemove', member => {
+    let myGuild = client.guilds.get('691904041634103306');
+    let memberCountChannel = myGuild.channels.get('701116524181717054') 
+    let memberCount = memberCountChannel.guild.memberCount;
+    memberCountChannel.setTopic(`**§ USUÁRIOS: ${memberCount}**`)
+    .catch(error => console.log(error))
+})
+client.on('guildMemberAdd', member => {
+    let myGuild = client.guilds.get('691904041634103306');
+    let memberCountChannel = myGuild.channels.get('701116524181717054') 
+    let memberCount = memberCountChannel.guild.memberCount;
+    memberCountChannel.setTopic(`**§ USUÁRIOS: ${memberCount}**`)
+    .catch(error => console.log(error))
+})
+
 client.on('message', message => { // nome desse evento, foi setado como: message
     if (message.author.bot) return; // puxando o nome definido, bloquearemos o uso de comandos por outros bots
     if (message.channel.type === "dm") return; // caso seja uma mensagem privada ao nosso bot, não retornaremos
 
     let prefix = config.prefix; // puxando o prefixo do nosso bot
-    if (!message.content.startsWith(prefix)) return; // para evitar bugs, setaremos uma function, definindo que o bot respondera apenas para mensagens que possuem seu prefixo, no inicio
-    var args = message.content.substring(config.prefix.length).split(" "); // definindo o que seria os argumentos
-    let cmd = args.shift().toLowerCase(); // puxando dos args, setaremos que o bot pode responder sim, a comandos com a letra inicial maiuscula
+    var args = message.content.substring(config.prefix.length).split(" ");
+    if (!message.content.startsWith(config.prefix)) return;
+     let cmd = args.shift().toLowerCase();
+     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    let command = client.commands.get(cmd) // puxaremos o conteudo de tal comando
-    if (command) { // caso o membro utilize um comando inexistente, daremos o erro
-    command.run(client, message, args); // essa é a base de todo arquivo js
+  let command =
+    client.commands.get(cmd) || client.commands.get(client.aliases.get(cmd));
+  if (command) {
+    command.run(client, message, args);
   } else {
-    message.reply(`hmmm :thinking: Não encontrei esse comando.`); // mensagem de comando inexistente
+    message.reply(
+      `não reconheci esse comando <:thinku:698536163123789884>`
+    );
   }
-})
+});
 
-client.login(config.token);
+client.login(process.env.TOKEN);
